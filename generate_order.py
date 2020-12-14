99f2973306ec15b7a873e66b5c798f0e29dca935
jmschabdach <jmschabdach@gmail.com>
2020-12-14 09:34:59 -0500

    added functionality for parsing command line argument

--- generate_order.py
+++ generate_order.py
@@ -1,7 +1,14 @@
 import argparse
 
 def main():
-    print("Hello world")
+    # Set up the command line argument parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--file', help="The location of the names file", default="./names.txt")
+    
+    # Parse the arguments
+    args = parser.parse_args()
+
+    print(args)
 
 if __name__ == "__main__":
     main()
